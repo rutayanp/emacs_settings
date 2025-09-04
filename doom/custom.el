0c3d5183e8610a5488fb8bda8de6f83f71680635
Rutayan <rutayanp@gmail.com>
2025-09-04 17:23:45 -0400

    Update custom.el

--- doom/custom.el
+++ doom/custom.el
@@ -1,12 +1,12 @@
-;;; -*- lexical-binding: t -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8"
-     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1" default)))
+   '("5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6" "83550d0386203f010fa42ad1af064a766cfec06fc2f42eb4f2d89ab646f3ac01" "9b9d7a851a8e26f294e778e02c8df25c8a3b15170e6f9fd6965ac5f2544ef2a9" "e7ce09ff7426c9a290d06531edc4934dd05d9ea29713f9aabff834217dbb08e4" "5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5" "b99ff6bfa13f0273ff8d0d0fd17cc44fab71dfdc293c7a8528280e690f084ef0" "2ab8cb6d21d3aa5b821fa638c118892049796d693d1e6cd88cb0d3d7c3ed07fc" "0f1341c0096825b1e5d8f2ed90996025a0d013a0978677956a9e61408fcd2c77" "4a4bf2300b5170ac1fdc4ecc02e7a6e31e4e2a7f4a6ac3c06f1d1fada8f9a56e" "9b55271bec4b2bae7eca6c96eac974b19f2f6d8cccc8fd34b30ab67220bb19d5" "c5801b68568b59976a8e58104c40c9b052d46cca72e367c2e43c1f36a9e79abb" "22a0d47fe2e6159e2f15449fcb90bbf2fe1940b185ff143995cc604ead1ea171" "a56cc18045d90be8f770ae409fc86274f8e5de2999a16b604ff84f8015e8d1e5" "bb5b5cec7ef86edbb6486c46ac7dd47694040951f5fe2cf7cbff537eca5dcc87" "7ec8fd456c0c117c99e3a3b16aaf09ed3fb91879f6601b1ea0eeaee9c6def5d9" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d" "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326" "b7a09eb77a1e9b98cafba8ef1bd58871f91958538f6671b22976ea38c2580755" default))
+ '(package-selected-packages
+   '(aircon-theme simplezen code-cells 0blayout darcula-theme jetbrains-darcula-theme flatland-theme treemacs all-the-icons all-the-icons-gnus all-the-icons-ibuffer all-the-icons-ivy-rich all-the-icons-nerd-fonts)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
